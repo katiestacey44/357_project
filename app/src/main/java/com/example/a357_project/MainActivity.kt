6b112f2bce98d41caaf64013f06ddc190c0ef2a4
katiestacey44 <katiestacey44@gmail.com>
2024-10-09 12:45:44 -0400

    attempt to get API working

--- app/src/main/java/com/example/a357_project/MainActivity.kt
+++ app/src/main/java/com/example/a357_project/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.a357_project
 
+import android.os.AsyncTask
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.RelativeLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,10 +14,27 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
 
     val CITY: String = "Allendale, MI"
-    //API  8b623b3c84a84f89b2c124733242409
+    val API: String = "8b623b3c84a84f89b2c124733242409"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        weatherTask().execute()
+    }
+    inner class weatherTask() : AsyncTask<String, Void, String >(){
+        override fun onPreExecute() {
+            super.onPreExecute()
+            findViewById<ProgressBar>(R.id.loader).visibility = View.VISIBLE
+            findViewById<RelativeLayout>(R.id.mainContainer).visibility = View.GONE
+            findViewById<TextView>(R.id.errortext).visibility = View.GONE
+        }
+        override fun doInBackground(vararg p0:String?): String{
+            var response:String?
+            try{
+               // response = URL("https")
+            }
+        }
+
     }
 }
