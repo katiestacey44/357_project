72ed1edc144b7143f2f9816cd12b2933864641e3
katiestacey44 <katiestacey44@gmail.com>
2024-10-09 12:24:58 -0400

    started on MainActivity

--- app/src/main/java/com/example/a357_project/MainActivity.kt
+++ app/src/main/java/com/example/a357_project/MainActivity.kt
@@ -8,26 +8,11 @@ import androidx.core.view.WindowInsetsCompat
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    val CITY: String = "Allendale, MI"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
-        // Handling insets for system bars
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
-        // Set up the UI for current weather and forecast
-       /* val greetingTextView: TextView = findViewById(R.id.greetingTextView)
-        greetingTextView.text = "Hello 357's Weather App!"
-
-        val currentWeatherTextView: TextView = findViewById(R.id.currentWeatherTextView)
-        currentWeatherTextView.text = "Current Weather: Sunny, 75°F"
-
-        val forecastTextView: TextView = findViewById(R.id.forecastTextView)
-        forecastTextView.text = "Forecast: 80°F tomorrow, partly cloudy"*/
     }
 }
