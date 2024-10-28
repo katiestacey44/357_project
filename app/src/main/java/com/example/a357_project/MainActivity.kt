79c2c6a6487f3258839fb7ebf1e6077647f286b1
KatieStacey <katiestacey44@gmail.com>
2024-10-28 13:35:07 -0400

    tasks created

--- app/src/main/java/com/example/a357_project/MainActivity.kt
+++ app/src/main/java/com/example/a357_project/MainActivity.kt
@@ -34,6 +34,27 @@ http://api.weatherapi.com/v1/astronomy.json?key=1fc460243bd649d898c163818242810&
 WeatherApp Login
 Username: Katiestacey44@gmail.com
 Password: Project357
+ */
+/*
+task 1: connect to API
+task 2: ensure that API will update screen
+task 3: create search screen
+task 4: ensure that search screen will search locations
+Task 5: ensure that Search Screen will update weatherapp screen
+task 6: connect to firebase
+task 7: create register and login activity
+task 8: ensure that firebase will save registered users
+task 9: create firestore database for locations saved
+task 10: create a save location option on search screen
+task 11: ensure that locations are being saved in the forestore database
+task 12: ensure that the saved locations is displays on the screen
+task 13: create notification messages based on current weather
+task 14: create notification alerts
+
+
+
+additional tasks:
+make it look pretty
 
 
  */
