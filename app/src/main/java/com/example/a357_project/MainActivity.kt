1fca8e2142a65fad4b73c9162bd03a07034d4141
KatieStacey <katiestacey44@gmail.com>
2024-10-30 10:19:39 -0400

    added more tasks

--- app/src/main/java/com/example/a357_project/MainActivity.kt
+++ app/src/main/java/com/example/a357_project/MainActivity.kt
@@ -39,17 +39,20 @@ Password: Project357
 task 1: connect to API
 task 2: ensure that API will update screen
 task 3: create search screen
-task 4: ensure that search screen will search locations
-Task 5: ensure that Search Screen will update weatherapp screen
-task 6: connect to firebase
-task 7: create register and login activity
-task 8: ensure that firebase will save registered users
-task 9: create firestore database for locations saved
-task 10: create a save location option on search screen
-task 11: ensure that locations are being saved in the forestore database
-task 12: ensure that the saved locations is displays on the screen
-task 13: create notification messages based on current weather
-task 14: create notification alerts
+task 4: make search screen contents and connections to desired other activities
+task 5: ensure that search screen will search locations
+Task 6: ensure that Search Screen will update weatherapp screen
+task 7: connect to firebase
+task 8: create register and login activity
+task 9: make contents for register screen
+task 10: make contents for login screen
+task 11: ensure that firebase will save registered users
+task 12: create firestore database for locations saved
+task 13: create a save location option on search screen
+task 14: ensure that locations are being saved in the firestore database
+task 15: ensure that the saved locations is displays on the screen
+task 16: create notification messages based on current weather
+task 17: create notification alerts
 
 
 
